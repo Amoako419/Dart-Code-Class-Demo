7cd8b0b128886dcb3169b13bcbd1de302cade482
amoako419 <ghheskey@gmail.com>
2024-02-13 09:17:23 +0000

    First commit

---
+++ first.dart
@@ -0,0 +1,39 @@
+//1.A code that will print out the largest of the three integer values
+//2.A code that will print out a set of given numbers between 40 and 100
+//3.A code that will print out an exam grade for an exam score
+void main(){
+// var num1 = 100;
+// var num2 = 200;
+// var num3 = 300;
+
+// if (num1>num2){
+//   print(num1);
+//   }
+//   else if (num1>num3){
+//     print(num1);
+// }
+// else if (num2>num3){
+//     print(num2);
+// }
+// else if (num3>num1){
+//     print(num3);}
+
+
+//2
+for (var i = 40;i < 100;i++){
+  if(i%2 == 0){
+  print(i);}
+}
+
+var x = 0;
+while (x < 10){
+  print(x*2);
+  x++;
+}
+   
+   
+
+
+}
+
+
