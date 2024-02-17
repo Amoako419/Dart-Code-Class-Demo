8bcf654060e7ee945e365582509da561e7b84159
Amoako Heskey <113794430+Amoako419@users.noreply.github.com>
2024-02-17 11:32:22 +0000

    2D lists in dart

---
+++ 2D-lists.dart
@@ -0,0 +1,14 @@
+void main(){
+  var a = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+
+  ];
+
+  for (var i = 0; i < a.length; i++) {
+   for (var j = 0; j < a.length; j++) {
+    print(a[i][j]);
+  }
+  }
+}
